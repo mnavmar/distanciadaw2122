f182eca8e71daa94e20b01887b2b260570a9205b
mnavmar <106330458+mnavmar@users.noreply.github.com>
2022-05-26 08:45:25 -0700

    Add files via upload

---
+++ practica-navarro.php
@@ -0,0 +1,33 @@
+<?php
+/**
+  *  @author navarro
+  *  @version 1.0
+*/
+
+  /**
+    * Función cuadraro
+    * @param int $num1
+    * @return float
+  */
+
+   function cuadrado($núm){
+    return $núm * $núm;
+}
+echo cuadrado(4); 
+
+  /**
+    * Función FamiliaNombre
+    * @param varchar2 $name
+    * @return varchar2
+  */
+  
+   function FamiliaNombre ($name){
+  	FamiliaNombre("$name");
+  	return $name
+}
+
+FamiliaNombre("Navarro");
+echo "$name Navarro"
+
+@internal: Solo para desarrolladores.
+?>
